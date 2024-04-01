91217b3994c1f75fd1590f6964d1625f853a9a96
Edwin Awariyah <edwin.awariyah@outlook.com>
2024-04-01 11:43:06 +0000

    LGTM

--- ViewImageAsArray.py
+++ ViewImageAsArray.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # image = cv2.imread('OriginalImage.jpg', cv2.IMREAD_GRAYSCALE)
-image = cv2.imread('TheFiveImage.jpeg', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('OriginalImage.jpg', cv2.IMREAD_GRAYSCALE)
 
 
 # min_intensity = np.min(image)
